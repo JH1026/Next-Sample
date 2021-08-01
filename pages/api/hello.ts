d17038dc1acaaed3daa12d3787da9bf0fb63afea
JuneOrg2020 <jh.dev.acc.1026.i2k9@gmail.com>
2021-08-01 13:28:39 +0900

    api test

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -5,9 +5,12 @@ type Data = {
   name: string
 };
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
 ) {
-  res.status(200).json({ name: 'John Doe' });
+  const fetchData = await fetch('https://www.googleapis.com/books/v1/volumes?q=React');
+  const data = await fetchData.json();
+
+  res.status(200).json(data);
 }
